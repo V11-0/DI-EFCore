d3d45b289bde98569e5ecfb377547269aa198773
Vinicius Bruscagini <vinibrenobr11@hotmail.com>
2021-12-08 20:00:13 -0300

    Initial Commit

---
+++ DI-EFCore.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DI_EFCore.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
